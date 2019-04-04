3d05fa71d2e5b07b6ba5d068a963c7030ed0c45e
Ani Turner <aniturner93@gmail.com>
2019-04-04 17:33:57 -0600

    hidden key

--- client/src/firebase/index.js
+++ client/src/firebase/index.js
@@ -1,16 +1,16 @@
 import firebase from 'firebase/app';
 import 'firebase/storage';
 
-// Initialize Firebase
-var config = {
-    apiKey: process.env.REACT_APP_APIKEY,
-    authDomain: "programory-app.firebaseapp.com",
-    databaseURL: "https://programory-app.firebaseio.com",
-    projectId: "programory-app",
-    storageBucket: "programory-app.appspot.com",
-    messagingSenderId: "100727656362"
-};
-firebase.initializeApp(config);
+ // Initialize Firebase
+ var config = {
+    apiKey: "AIzaSyA6n_nF9Qgveh_a0VULmgeHmEz6R6_lHRM",
+    authDomain: "programory-app1.firebaseapp.com",
+    databaseURL: "https://programory-app1.firebaseio.com",
+    projectId: "programory-app1",
+    storageBucket: "",
+    messagingSenderId: "406730369213"
+  };
+  firebase.initializeApp(config);
 
 const storage = firebase.storage();
 
